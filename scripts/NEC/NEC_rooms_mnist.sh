78a0ced2cf3c113ae572ecb8e4c9435d84a20269
jlrussin <jlrussin@gmail.com>
2020-03-08 18:36:01 -0700

    shorter training for testing bugs

--- scripts/NEC/NEC_rooms_mnist.sh
+++ scripts/NEC/NEC_rooms_mnist.sh
@@ -25,7 +25,7 @@ python train.py \
 --room_size 9 \
 --fourrooms_state_type mnist \
 --frames_to_stack 1 \
---training_frames 1000000 \
+--training_frames 10 \
 --initial_epsilon 0.1 \
 --final_epsilon 0.1 \
 --epsilon_decay 1.0 \
